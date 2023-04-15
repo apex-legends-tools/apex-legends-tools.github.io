65faeb3ea59fcb6f339f9e0547c1fb8fdaec224b
syan <truesyan@gmail.com>
2023-04-15 16:55:42 +1000

    Add link to apex-bravery

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <main>
+      <Link href="/apex-bravery">Apex Bravery</Link>
     </main>
   )
 }
